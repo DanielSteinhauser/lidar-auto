09a01d4341df23264bf31bd789dff30792b780c2
mnuluc <baier-manuel@gmx.de>
2021-12-03 14:45:58 +0100

    Webserver schönerer Code

--- src/Main/Main.ino
+++ src/Main/Main.ino
@@ -2,28 +2,13 @@
 #include "Display.h"
 #include "Steuerung.h"
 #include "Webserver.h"
- bool server_en = true;
 
 void setup() {
   kurz = false;
   Serial.begin(115200);
   Serial2.begin(115200,SERIAL_8N1,22,23);
-  
   stepper.setSpeed(5);
-  
-  if (server_en){   //Webserver eingeschaltet
-      WiFi.begin(ssid, password);
-     while (WiFi.status() != WL_CONNECTED) 
-      {  delay(500);
-         Serial.print(".");
-      }
-      Serial.println("");
-      Serial.println("WiFi connected.");
-      Serial.println("IP address: ");
-      Serial.println(WiFi.localIP());
-      server.begin();
-  }
-  
+  webserverInit();
   displayInit();
 }
 
--- src/Main/Webserver.h
+++ src/Main/Webserver.h
@@ -6,3 +6,20 @@ const char* password = "REPLACE_WITH_YOUR_PASSWORD";
 WiFiServer server(80);
 
 String header;
+
+boolean  server_en = true;
+
+void webserverInit(){
+  if (server_en){   //Webserver eingeschaltet
+      WiFi.begin(ssid, password);
+     while (WiFi.status() != WL_CONNECTED) 
+      {  delay(500);
+         Serial.print(".");
+      }
+      Serial.println("");
+      Serial.println("WiFi connected.");
+      Serial.println("IP address: ");
+      Serial.println(WiFi.localIP());
+      server.begin();
+  }
+}
