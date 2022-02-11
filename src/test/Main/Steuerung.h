5d8b781d6c93532f7f39b2323e3f7cf5afaf14a5
DanielSteinhauser <72268029+DanielSteinhauser@users.noreply.github.com>
2022-02-11 15:02:00 +0100

    yes

--- src/test/Main/Steuerung.h
+++ src/test/Main/Steuerung.h
@@ -1,8 +1,5 @@
 //Steuert Schrittmotor, Motoren
 #include <Stepper.h>
-int sped = 100;
-int aus = 0;
-
 
 Stepper stepper (4096,IN1, IN3, IN2, IN4);
 
@@ -18,24 +15,24 @@ void stepperBewegen(boolean uhrzeigersinn){
 }
 
 void geradeausfahren(){
- ledcWrite(0, sped);
- ledcWrite(1, aus);
- ledcWrite(2, aus);
- ledcWrite(3, sped);
+ digitalWrite(12, HIGH);
+ digitalWrite(14, LOW);
+ analogWrite(26, LOW);
+ analogWrite(27, HIGH);
 }
 
 void anhalten(){
- ledcWrite(0, aus);
- ledcWrite(1, aus);
- ledcWrite(2, aus);
- ledcWrite(23, aus);
+ digitalWrite(12, LOW);
+ digitalWrite(14, LOW);
+ digitalWrite(26, LOW);
+ digitalWrite(27, LOW);
 }
 
 void rueckwaertsfahren(){
-  ledcWrite(12, aus);
-  ledcWrite(14, sped);
-  ledcWrite(26, sped);
-  ledcWrite(27, aus);
+ digitalWrite(12, LOW);
+ digitalWrite(14, HIGH);
+ digitalWrite(26, HIGH);
+ digitalWrite(27, LOW);
  delay(3000);
  anhalten();
 }
@@ -43,17 +40,17 @@ void rueckwaertsfahren(){
 void kurvefahren(double n){
   double time = fabs(n) * 8.333;
   if(n > 0){
-  ledcWrite(12, aus);
-  ledcWrite(14, aus);
-    ledcWrite(26, aus);
-    ledcWrite(27, sped);
+  digitalWrite(12, LOW);
+  digitalWrite(14, LOW);
+    digitalWrite(26, LOW);
+    digitalWrite(27, HIGH);
   delay(time);
   anhalten();
   }else{
-    ledcWrite(12, 100);
-    ledcWrite(14, aus);
-    ledcWrite(26, aus);
-    ledcWrite(27, aus);
+    digitalWrite(12, HIGH);
+    digitalWrite(14, LOW);
+    digitalWrite(26, LOW);
+    digitalWrite(27, LOW);
   delay(time);
   anhalten();
 }
