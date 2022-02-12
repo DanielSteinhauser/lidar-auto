453066afa82e19cbe5a954967c249c19268f1243
mnuluc <baier-manuel@gmx.de>
2022-02-12 18:25:38 +0100

    Umsetzung Zustände, Implementierung MQTT

--- src/Main/Main.ino
+++ src/Main/Main.ino
@@ -1,10 +1,17 @@
-boolean  server_en = false; // Webserver Hauptschalter
+boolean  server_en = true; // Webserver Hauptschalter
+
+int zustand = 0; //Startzustand
+// zustand 0: warten auf befehle, ruhe
+// zustand 1: autonomes fahren
+// zustand 2: manuelle Steuerung
+
 
 #include "Lidar.h"
 #include "Display.h"
 #include "Webserver.h"        // Reihenfolge hier wichtig weil C-Compiler = geistig behindert
 #include "Steuerung.h"
 #include "Scan.h"
+
 
 void setup() {
   ledcSetup(0, 5000, 8);
@@ -22,43 +29,65 @@ void setup() {
   displayInit();
   if (server_en) webserverInit();
   motorInit();
+
+  
+  clearDisplay();
+  aufDisplayAnzeigen(0, 0, "Startzustand:");
+  aufDisplayAnzeigen(0, 10, String(zustand));
+  delay(1000);
 }
 
 void loop() {
-  dauerScan(15);
-  displayDatenSchreiben();
-
-  if(kurz == false){
-    geradeausfahren();
-  } 
-
-  if(kurz){
-    scan(45);
-    // Wenn immer noch Kurz 
-    if(kurz == true){
-      rueckwaertsfahren();
-    }
-    //Wenn Kurvemöglichkeit gefunden
-    else if(kurz == false){
-      // Wenn Rechtsdistanz größer wie Linksdistanz
-      if (rd > ld){ 
-        kurvefahren(r);
-      }
-      // Wenn Linksdistanz größer wie Rechtsdistanz
-      else if(ld > rd){
-        kurvefahren(l);
-      }
-      // Wenn beide gleich groß sind
-      else{
-        kurvefahren(r);
-      } 
-    }  
-    // Nach der Kurve weiter fahren
-    geradeausfahren();
-    rd = 0;
-    ld = 0;
-    r = 0;
-    l = 0;  
-  }
   
+  MQTTabrufen();
+
+  switch (zustand){
+     case 0: // ruhezustand
+              anhalten();
+              
+              break;
+
+              
+     case 1:  // autonomes fahren
+              dauerScan(15);
+              displayDatenSchreiben();
+            
+              if(kurz == false){
+                geradeausfahren();
+              } 
+            
+              if(kurz){
+                scan(45);
+                // Wenn immer noch Kurz 
+                if(kurz == true){
+                  rueckwaertsfahren();
+                }
+                //Wenn Kurvemöglichkeit gefunden
+                else if(kurz == false){
+                    // Wenn Rechtsdistanz größer wie Linksdistanz
+                    if (rd > ld){ 
+                      kurvefahren(r);
+                    }
+                    // Wenn Linksdistanz größer wie Rechtsdistanz
+                    else if(ld > rd){
+                      kurvefahren(l);
+                    }
+                    // Wenn beide gleich groß sind
+                    else{
+                      kurvefahren(r);
+                    } 
+                }
+                // Nach der Kurve weiter fahren
+                geradeausfahren();
+                rd = 0;
+                ld = 0;
+                r = 0;
+                l = 0;  
+              }
+              break;
+              
+     case 2:  
+            anhalten(); // PLACEHOLDER
+            break; //manuelles fahren
+  }
 }
